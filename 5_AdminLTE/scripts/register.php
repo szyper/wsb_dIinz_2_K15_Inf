9c8bc219e87b9cc9ef657651dec5e5b432f2379b
szyper <zskgit@gmail.com>
2023-05-17 13:56:18 +0200

    register_user

--- 5_AdminLTE/scripts/register.php
+++ 5_AdminLTE/scripts/register.php
@@ -1,50 +1,76 @@
 <?php
+function sanitizeInput(&$input){
+	$input = trim($input);
+	$input = stripslashes($input);
+	$input = htmlentities($input);
+	return $input;
+}
+
+//echo $_POST["firstName"]." ==> ".sanitizeInput($_POST["firstName"]).", ilość znaków: ".strlen($_POST["firstName"]);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
 //	echo "<pre>";
 //		print_r($_POST);
 //	echo "</pre>";
 
+	$required_fields =["firstName", "lastName", "email1", "email2", "pass1", "pass2", "birthday", "city_id", "gender"];
+
+//	foreach ($required_fields as $key => $value){
+//		echo "$key: $value<br>";
+//	}
+
 	session_start();
-	foreach ($_POST as $key => $value){
-		if (empty($value)){
-			$_SESSION["error"] = "Wypełnij wszystkie pola!";
-			echo "<script>history.back();</script>";
-			exit();
+//	foreach ($_POST as $key => $value){
+//		if (empty($value)){
+//			$_SESSION["error"] = "Wypełnij wszystkie pola!";
+//			echo "<script>history.back();</script>";
+//			exit();
+//		}
+//	}
+
+	$errors = [];
+
+	foreach ($required_fields as $value){
+		if (empty($_POST[$value])){
+			$errors[] = "Pole <b>$value</b> jest wymagane!";
 		}
 	}
 
-	$error = 0;
-
-	//regulamin
-	if (!isset($_POST["terms"])){
-		$error = 1;
-		$_SESSION["error"] = "Zatwierdź regulamin!";
+	if (!empty($errors)){
+		$_SESSION["error"] = implode("<br>", $errors);
+		echo "<script>history.back();</script>";
+		exit();
 	}
 
-	if (!isset($_POST["gender"])){
-		$error = 1;
-		$_SESSION["error"] = "Zaznacz płeć!";
+	//email
+	if ($_POST["email1"] != $_POST["email2"])
+		$errors[] = "Adresy email są różne!";
+
+	if ($_POST["additional_email1"] != $_POST["additional_email2"]){
+		$errors[] = "Adresy dodatkowe email są różne!";
+	}else{
+		if (empty($_POST["additional_email1"]))
+			$_POST["additional_email1"] = NULL;
 	}
 
 	//hasło
 	if ($_POST["pass1"] != $_POST["pass2"]){
-		$error = 1;
-		$_SESSION["error"] = "Hasła są różne!";
+		$errors[] = "Hasła są różne!";
+	}else{
+		//walidacja hasła
+		if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POST["pass1"])) {
+			$errors[] = "Hasło nie spełnia wymagań!";
+		}
 	}
 
-	//email
-	if ($_POST["email1"] != $_POST["email2"]){
-		$error = 1;
-		$_SESSION["error"] = "Adresy email są różne!";
-	}
+	if (!isset($_POST["gender"]))
+		$errors[] = "Zaznacz płeć!";
 
-	//walidacja hasła
+	if (!isset($_POST["terms"]))
+		$errors[] = "Zatwierdź regulamin!";
 
-if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POST["pass1"])) {
-	$error = 1;
-	$_SESSION["error"] = "Hasło nie spełnia wymagań!";
-}
-
-	if ($error != 0){
+	if (!empty($errors)){
+		$_SESSION["error"] = implode("<br>", $errors);
 		echo "<script>history.back();</script>";
 		exit();
 	}
@@ -61,13 +87,18 @@ if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POS
 		exit();
 	}
 
-	$stmt = $conn->prepare("INSERT INTO `users` (`email`, `city_id`, `firstName`, `lastName`, `birthday`, `gender`, `avatar`, `password`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, current_timestamp());");
+	foreach ($_POST as $key => $value){
+		if (!$_POST["pass1"] && !$_POST["pass2"])
+			sanitizeInput($_POST["$key"]);
+	}
+
+	$stmt = $conn->prepare("INSERT INTO `users` (`email`, `additional_email`, `city_id`, `firstName`, `lastName`, `birthday`, `gender`, `avatar`, `password`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, current_timestamp());");
 
 	$pass = password_hash('$_POST["pass1"]', PASSWORD_ARGON2ID);
 
 	$avatar = ($_POST["gender"] == 'm') ? './jpg/man.png' : './jpg/woman.png';
 
-	$stmt->bind_param('sissssss', $_POST["email1"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $_POST["gender"], $avatar, $pass);
+	$stmt->bind_param('ssissssss', $_POST["email1"], $_POST["additional_email1"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $_POST["gender"], $avatar, $pass);
 
 	$stmt->execute();
 
@@ -76,5 +107,6 @@ if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POS
 	}else{
 		$_SESSION["error"] = "Nie udało się dodać użytkownika!";
 	}
+}
 
-	header("location: ../pages/register.php");
+header("location: ../pages/register.php");
